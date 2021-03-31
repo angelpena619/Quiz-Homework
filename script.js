28bea13fd678d0bce5a6fb41ceee19d410c57b32
Angel <angelpena619@gmail.com>
2021-03-30 17:13:09 -0700

    this is too hard

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log (document);
+
+var quuestionsContainer = document.getElementById("questions-container");
+var quuestionTitles = document.getElementById("question-titles");
+var quuestions = document.getElementById("questions");
+var quuestions = document.getElementById("questions");
+
+var startQuiz = document.getElementById("start-button");
+var answer1 = document.getElementById("button1");
+var answer2 = document.getElementById("button2");
+var answer3 = document.getElementById("button3");
+var answer4 = document.getElementById("button4");
+
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
+] 
