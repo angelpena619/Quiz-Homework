e383b12bd91bea29f56cd7907c745603182ae56f
Angel <angelpena619@gmail.com>
2021-04-01 18:17:33 -0700

    adding updates to JS file

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
-
 var startQuiz = document.getElementById("start-button");
-
+var questionTitles=document.getElementById("question-title");
+var question_options=document.getElementsByClassName("answer-button");
 var current_question_index = 0;
 
-const questionWrapper = document.querySelector(".questions");
-document.querySelector(".questions").innerHTML =
 
-const questions = {
+//questions
+const questions = [{
     title: 'What color is the sky?',
     options: ['red', 'yellow', 'blue'],
     answer: 'blue' 
-  }
-  const question = {
+  },
+{
     title: 'what day is today?',
     options: ['mon', 'tues', 'weds'],
     answer: 'weds' 
-  }
-  const question = {
+  },
+ {
     title: 'Who is the prez?',
     options: ['clinton', 'bush', 'bidon'],
     answer: 'bidon' 
-  }
+  }]
+  var current_question = questions[current_question_index];
 
   function displayCurrentQuestion() {
-    var current_question = question[current_question_index];
-   }
+    current_question = questions[current_question_index];
+    questionTitles.textContent=current_question.title
+    var i;
+  for (i = 0; i < current_question.options.length; i++) {
+  question_options[i].textContent=current_question.options[i]
 
-   funtion checkAnswers(){
-       var question1 = document.getElementById.(#red);
-       var question1 = document.getElementById.(#yellow);
-       var question1 = document.getElementById.(#blue);
+}
 
-if (question1 == "blue") {
-    correct++;
-}
-if (question2 == "weds") {
-    correct++;
-}
-if (question2 == "bidon") {
-    correct++;
-}
+  }
+  document.addEventListener("click",handleAnswerquestion)
+
+  function handleAnswerquestion (event){
+    console.log('here')
+    if (event.target.matches(".answer-button")) {
+      console.log("there")
+      if(event.target.textContent===current_question.answer){
+        console.log('here')
+        current_question_index++
+        displayCurrentQuestion()
+      }
+    }
+
+  }
+
+
+document.querySelector("#start").addEventListener("click", displayCurrentQuestion);
+
+
+//var current_question = questions[current_question_index];
+   // var p = document.createElement("p");
+    //p.textContent = current_question.title; 
+   // document.querySelector("#currentQuestion").appendChild(p);
