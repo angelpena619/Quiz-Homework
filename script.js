618f4bd55c9e33601507b70e202538bba67c07eb
Angel <angelpena619@gmail.com>
2021-04-03 17:22:51 -0700

    last commit

--- script.js
+++ script.js
@@ -4,20 +4,39 @@ var question_options_A = document.getElementById("answer-button-A");
 var question_options_B = document.getElementById("answer-button-B");
 var question_options_C = document.getElementById("answer-button-C");
 var current_question_index = 0;
+var timeLeft = 75;
+
+
+/*startButton.addEventListener("click", function() {
+  startTime();
+  })
+
+  function startTime() {
+    timerInterval = setInterval(function() {
+      if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        endGame();
+    }
+    timeLeft--;
+    timer.textContent = "Time left: " + timeLeft;
+
+  }, 1000)
+*/
+
 
 const questions = [
   {
-    title: "What color is the sky?",
-    options: ["red", "yellow", "blue"],
+    title: "What does the DOM stand for?",
+    options: ["Days on market", "Domino", "Document Object Model"],
     answer: 2,
   },
   {
-    title: "What day is today?",
-    options: ["mon", "tues", "weds"],
+    title: "What does CSS stand for?",
+    options: ["Cats Somtimes See", "Catagory Series Selector", "Cascading Style Sheets"],
     answer: 2,
   },
   {
-    title: "Who is the prez?",
+    title: "What are AP?",
     options: ["clinton", "bush", "bidon"],
     answer: 2,
   },
@@ -29,27 +48,21 @@ document
   .querySelector("#start")
   .addEventListener("click", displayCurrentQuestion);
 
-function displayCurrentQuestion() {
-  console.log (questions[current_question_index])
-  current_question = questions[current_question_index];
-  questionTitles.innerHTML = current_question.title;
-
-  for (var i = 0; questions[current_question_index].options.length; i++) {
-
-    question_options_A.innerHTML = current_question.options[i];
-    question_options_B.innerHTML = current_question.options[i];
-    question_options_C.innerHTML = current_question.options[i];
+  function displayCurrentQuestion() {
+    document.querySelector(".questions").setAttribute("class","questions show")
+    current_question = questions[current_question_index];
+    questionTitles.innerHTML = current_question.title;
+    question_options_A.innerHTML = current_question.options[0];
+    question_options_B.innerHTML = current_question.options[1];
+    question_options_C.innerHTML = current_question.options[2];
   }
-}
-//*document.addEventListener("click", handleAnswerquestion);
 
-function handleAnswerquestion(event) {
-  if (event.target.matches(".answer-button")) {
-    if (event.target.textContent === current_question.answer) {
-      current_question_index++;
-      displayCurrentQuestion();
+  function checkAnswers(answer) {
+    correct = questions[current_question_index++].answer;
+    if (answer === answer && current_question_index++){
+      alert("that is correct!");
     }
-  }
-}
 
-function checkAnswers(answer) {}
+  
+    
+  }
