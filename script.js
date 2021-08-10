d8887ba55ac94fc0e340bc0f2cf600a1cec8d25c
Angel <angelpena619@gmail.com>
2021-08-09 19:01:20 -0700

    fixing files

--- script.js
+++ script.js
@@ -1,69 +1,215 @@
-var startQuiz = document.getElementById("start-button");
-var questionTitles = document.getElementById("question-title");
-var question_options_A = document.getElementById("answer-button-A");
-var question_options_B = document.getElementById("answer-button-B");
-var question_options_C = document.getElementById("answer-button-C");
-var current_question_index = 0;
-var timeLeft = 75;
+function hi() {
+  document.getElementById("myScore").innerHTML = "";
+}
+function hideStart() {
+  let x = document.getElementById("start_quiz");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function startQuiz() {
+  let x = document.getElementsByClassName("quiz-container")[0];
 
-//---was unsucesull detting up the interval counter but i did attempt so this is commented out
+  let z = document.getElementById("next");
+  if (x.style.display === "none") {
+    x.style.display = "block";
 
-/*startButton.addEventListener("click", function() {
-  startTime();
-  })
+    z.style.visibility = "visible";
+  }
+}
+let timerCount = 75;
 
-  function startTime() {
-    timerInterval = setInterval(function() {
-      if (timeLeft <= 0) {
-        clearInterval(timerInterval);
-        endGame();
+function initialTimer() {
+  setInterval(function () {
+    let x = document.getElementById("myTimer");
+    x.innerText = `Time remaining ${timerCount}`;
+    if (timerCount > 0) {
+      timerCount = timerCount - 1;
+    } else {
+      showSlide(slides.length - 1);
     }
-    timeLeft--;
-    timer.textContent = "Time left: " + timeLeft;
+  }, 1000);
+}
 
-  }, 1000)
-*/
+function wrongAnswerTimer() {
+  let x = document.getElementById("myTimer");
+  x.innerText = `Time remaining ${timerCount}`;
 
-//---below are tbe question arrays
-const questions = [
+  timerCount = timerCount - 10;
+}
+
+function buildQuiz() {
+  // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // variable to store the list of possible answers
+    const answers = [];
+
+    // and for each available answer...
+    for (letter in currentQuestion.answers) {
+      // ...add an HTML radio button
+      answers.push(
+        `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+      );
+    }
+
+    // add this question and its answers to the output
+    output.push(
+      `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+    );
+  });
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+}
+
+function showResults() {
+  // gather answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll(".answers");
+
+  // keep track of user's answers
+  let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // find selected answer
+
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if (userAnswer === currentQuestion.correctAnswer) {
+      // add to the number of correct answers
+      numCorrect = numCorrect + 10;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = "lightgreen";
+    }
+    else {
+
+      answerContainers[questionNumber].style.color = "red";
+    }
+  });
+
+  resultsContainer.innerHTML = `${numCorrect} out of ${
+    myQuestions.length * 10 - 10
+  }`;
+  let name = document.getElementById("name").value;
+  document.getElementById(
+    "myScore"
+  ).innerText = `${name}  ${numCorrect} out of ${myQuestions.length * 10 - 10}`;
+  document.getElementById("showScore").style.visibility = "visible";
+  let y = document.getElementById("goBack");
+  y.style.visibility = "visible";
+  document.getElementById("bigDiv").style.display = "none";
+}
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove("active-slide");
+  slides[n].classList.add("active-slide");
+  currentSlide = n;
+
+  if (currentSlide === slides.length - 1) {
+    nextButton.style.display = "none";
+  } else {
+    nextButton.style.display = "inline-block";
+  }
+  if (currentSlide === slides.length - 1) {
+    
+    let myName = document.getElementById("myName");
+    myName.style.visibility = "visible";
+    document.getElementById("myTimer").style.visibility = "hidden";
+  }
+}
+
+function showNextSlide() {
+  console.log(`Current slide is ${currentSlide}`);
+
+  const answerContainers = quizContainer.querySelectorAll(".answers");
+  const answerContainer = answerContainers[currentSlide];
+  const selector = `input[name=question${currentSlide}]:checked`;
+
+  const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  let answerType = document.getElementById("answer_type");
+  answerType.style.visibility = "visible";
+  console.log(timerCount);
+  if (userAnswer != myQuestions[currentSlide].correctAnswer) {
+    answerType.innerText = "Previous Answer was !Wrong";
+    console.log(`User answer is ${userAnswer}`);
+    console.log(`Correct answer is ${myQuestions[currentSlide].correctAnswer}`);
+
+    console.log(timerCount);
+    wrongAnswerTimer();
+  } else {
+    answerType.innerText = "Previous Answer was !Correct";
+  }
+
+  showSlide(currentSlide + 1);
+}
+
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+
+const myQuestions = [
   {
-    title: "What does the DOM stand for?",
-    options: ["Days on market", "Domino", "Document Object Model"],
-    answer: 2,
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Funsuk Sandberg",
+      c: "Brendan Eich",
+    },
+    correctAnswer: "c",
   },
   {
-    title: "What does CSS stand for?",
-    options: ["Cats Somtimes See", "Catagory Series Selector", "Cascading Style Sheets"],
-    answer: 2,
+    question: "Which one of these is a clone of JavaScript?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm",
+    },
+    correctAnswer: "b",
   },
   {
-    title: "What are AP?",
-    options: ["clinton", "bush", "bidon"],
-    answer: 2,
+    question: "Best Framework?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Quiz Over",
+    answers: {},
+    correctAnswer: "",
   },
 ];
 
-//question generator, I was having issues with this syntax im still not confident in my javascript abilities 
 
-document
-  .querySelector("#start")
-  .addEventListener("click", displayCurrentQuestion);
+buildQuiz();
 
-  function displayCurrentQuestion() {
-    document.querySelector(".questions").setAttribute("class","questions show")
-    current_question = questions[current_question_index];
-    questionTitles.innerHTML = current_question.title;
-    question_options_A.innerHTML = current_question.options[0];
-    question_options_B.innerHTML = current_question.options[1];
-    question_options_C.innerHTML = current_question.options[2];
-  }
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
 
-  function checkAnswers(answer) {
-    correct = questions[current_question_index++].answer;
-    if (answer === answer && current_question_index++){
-      alert("that is correct!");
-    }
+showSlide(currentSlide);
 
-  
-    
-  }
+submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", showResults);
+
+nextButton.addEventListener("click", showNextSlide);
